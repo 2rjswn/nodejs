7a4f1493352132da3a20bf900d51e5d5f4f6f984
2rjswn <h47841930@gmail.com>
2024-10-05 18:30:38 +0900

    server

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+let count = 0;
+
+const server = http.createServer((req, res) => {
+  console.log((count += 1));
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.write("hello\n");
+  setTimeout(() => {
+    res.end("Node.js");
+  }, 10000);
+});
+
+server.listen(8000, () => console.log("Hello Node.js"));
