551cdb08b7ab3fb56e8f08b289ff78dc7b7e0096
2rjswn <h47841930@gmail.com>
2024-10-10 03:02:07 +0900

    refactoring

--- express/express-server/HelloExpress.js
+++ express/express-server/HelloExpress.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 3001;
-app.get("/", (req, res) => {
-    res.set({"Content-Type": "text/html; charset=utf-8"});
-    res.end("헬로 express");
-});
+const url = require("url");
 
 app.listen(port, () =>{
-    console.log(`start server : user${port}`)
-})
+    console.log("라우터 리팩터링");
+});
+app.get("/", (_, res) => res.end("HOME"));
+app.get("/user", user);
+app.get("/feed", feed);
+function user(req, res) {
+    const user = url.parse(req.url, true).query;
+    res.json(`[user] name : ${user.name}, age:${user.age}`);
+}
+function feed(_, res) {
+    res.json(`<ul>
+        <li>picture1</li>
+        <li>picture2</li>
+        <li>picture3</li>
+        </ul>
+    `);
+}
