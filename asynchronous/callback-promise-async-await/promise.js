7ca700d5bc6f9957a0af060b5581fdf689202cb7
2rjswn <h47841930@gmail.com>
2024-10-15 12:21:38 +0900

    promise

---
+++ asynchronous/callback-promise-async-await/promise.js
@@ -0,0 +1,37 @@
+const DB = [];
+
+function saveDB(user) {
+    const oldDBSize = DB.length;
+    DB.push(user);
+    console.log(`save ${user.name} to DB`);
+    return new Promise((resolve, reject) => {
+        if (DB.length > oldDBSize) {
+            resolve(user);
+        } else {
+            reject(new Error("error!"));
+        }
+    });
+}
+
+function sendMail(user) {
+    console.log(`email to ${user.mail}`);
+    return new Promise((resolve) => {
+        resolve(user);
+    });
+}
+
+function getResult(user) {
+    return new Promise((resolve, reject) => {
+        resolve(`success register ${user.name}`);
+    });
+}
+
+function registerByPromise(user) {
+    const result = saveDB(user).then(sendMail).then(getResult);
+    console.log(result);
+    return result;
+}
+
+const myUser = {mail : "ddd", pwd : "dddd", name : "ddddd"};
+const result = registerByPromise(myUser)
+result.then(console.log)
